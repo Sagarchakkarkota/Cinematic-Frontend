b6555ebaf0a9e594648acc1a432d6cb245363df8
Sagarchakkarkota <Sagar.Chakkarkota@QuantaAssitaInc.onmicrosoft.com>
2026-08-16 14:45:47 +0530

    fix : style issues fixed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,15 @@
-import { AboutHero } from './_components/AboutHero'
-import { AboutStory } from './_components/AboutStory'
-import { AboutValues } from './_components/AboutValues'
+import { useEffect } from "react";
+import { AboutHero } from "./_components/AboutHero";
+import { AboutStory } from "./_components/AboutStory";
+import { AboutValues } from "./_components/AboutValues";
 
 export default function AboutPage() {
+  // useEffect
   return (
     <div className="pt-24 pb-16">
       <AboutHero />
       <AboutStory />
       <AboutValues />
     </div>
-  )
+  );
 }
